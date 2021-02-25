c2bb1485ec3feb88079974f1861de8c7f16ce29d
z1300a2k <z1300a2k@V4C526213180B.LVRINTERN.LVR.DE>
2021-02-25 16:20:12 +0100

    Problem mit Absätzen beim lesen der Datei mit BufferedReader

--- Praktikumsaufgaben/src/dateiArbeit/Start.java
+++ Praktikumsaufgaben/src/dateiArbeit/Start.java
@@ -1,103 +1,268 @@
 package dateiArbeit;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Reader;
+import java.io.Writer;
+import java.util.ArrayList;
 
 
 public class Start {
 
 	static File myFile = new File("C:\\Users\\z1300a2k\\Documents\\beispielDatei.txt");
 	static File myFile2 = new File("C:\\Users\\z1300a2k\\Documents\\beispielDateiSchreiben.txt");
+	static File myFile3 = new File("C:\\Users\\z1300a2k\\Documents\\beispielDateiSchreibenRückwärts.txt");
+	static File myFile4 = new File("C:\\Users\\z1300a2k\\Documents\\beispielDateiSchreibenRückwärtsZeilenweise.txt");
+	static File myFile5 = new File("C:\\Users\\z1300a2k\\Documents\\beispielDateiSchreibenROT13.txt");
 	
 	StringBuffer wholeText; //evtl. noch nützlich
 	
 	public static void main(String[] args) {
-		
-		System.out.println("EinlesenUndSchreibenVonText\n");
-		Start.EinlesenUndSchreibenVonText();
-		
+		String str = "   Das ist ein String\n ";
+		System.out.println(str);
+		System.out.println(str.trim());
+		System.out.println(str.trim());
+		Start.einlesenUndSchreibenVonText(Start.myFile, Start.myFile2);
 		System.out.println();
-		Start.gebeDateiZeilenweiseAus(Start.myFile);
+		Start.einlesenUndBuchstabenweiseRückwärtsSchreiben(Start.myFile2, Start.myFile3);
 		System.out.println();
-		Start.gebeDateiByteweiseAus(Start.myFile);
+		Start.einlesenUndZeilenweiseRückwärtsSchreiben(Start.myFile3, Start.myFile4);
+		System.out.println();
+		Start.einlesenUndMitROT13Schreiben(Start.myFile4, Start.myFile5);
+		System.out.println();
+		System.out.println();
+		
+		Start.gebeDateiZeichenweiseAus(Start.myFile);
 		System.out.println();
 		Start.gebeDateiZeichenweiseAus(Start.myFile2);
 		System.out.println();
-		Start.gebeDateiByteweiseAus(Start.myFile2);
+		Start.gebeDateiZeichenweiseAus(Start.myFile3);
+		System.out.println();
+		Start.gebeDateiZeichenweiseAus(Start.myFile4);
+		System.out.println();
+		Start.gebeDateiZeichenweiseAus(Start.myFile5);
+		System.out.println();
+		Start.gebeDateiROT13DecryptedAus(Start.myFile5);
+		System.out.println();
+		Start.gebeDateiZeichenweiseAusInDezimal(Start.myFile4);
+		System.out.println();
+		Start.gebeDateiZeichenweiseAusInDezimal(Start.myFile5);
 		
 		System.exit(0);
 	}
 	
+	private static void einlesenUndMitROT13Schreiben(File readFile, File writeFile) {
+		if (readFile.isFile() && readFile.canRead() && writeFile.canWrite()) {
+			
+			System.out.println("EinlesenUndMitROT13Schreiben");
+			System.out.println(readFile.length() + "-> Länge der Ursprungsdatei");
+			BufferedReader in = Start.generateBufferdReader(readFile);
+			char c = 0;
+			StringBuffer wholeTextRot13 =  new StringBuffer();
+			PrintWriter pw = Start.generatePrintWriter(writeFile);
+			try {
+				char abbruch = (char) -1;
+				while ((c = (char) in.read()) != abbruch) {
+					char cEncrypted = (char) (c + 13);
+					wholeTextRot13.append(cEncrypted);
+				}
+				
+				////////// HIER WERDEN ZWEI ZEICHEN ANGEHÄGT /////////
+				System.out.println(wholeTextRot13.length() + "-> Länge des zu Schreibenden Textes");
+				pw.print(wholeTextRot13);
+//				pw.write(wholeTextRot13.toString()); -> ändert nichts an der Zeichenanzahl in Datei
+				pw.flush();
+				System.out.println(writeFile.length() + "-> Länge der beschriebenen Datei");
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			finally {
+				Start.closeReader(in);
+				Start.closeWriter(pw);
+			}
+		}
+	}
 	
-	public static void EinlesenUndSchreibenVonText() {
-		FileReader fr = null;
-		FileWriter fw = null;
-		try { fr = new FileReader(Start.myFile); }
-		catch (FileNotFoundException e1) { e1.printStackTrace(); }
-		try { fw = new FileWriter("C:\\Users\\z1300a2k\\Documents\\beispielDateiSchreiben.txt"); } 
-		catch ( IOException e ) { System.err.println( "Konnte Datei nicht erstellen" ); }
-		
-		if (Start.myFile.isFile() && Start.myFile.canRead()) {
+	private static void einlesenUndZeilenweiseRückwärtsSchreiben(File readFile, File writeFile) {
+		if (readFile.isFile() && readFile.canRead() && writeFile.canWrite()) {
+			
+			System.out.println("EinlesenUndZeilenweiseRückwärtsSchreiben");
+			System.out.println(readFile.length() + "-> Länge der Ursprungsdatei");
+			BufferedReader in = Start.generateBufferdReader(readFile);
+			String zeile = "";
+			ArrayList<String> lineArray = new ArrayList<>();
+			StringBuffer linesBackward =  new StringBuffer();
+			PrintWriter pw = Start.generatePrintWriter(writeFile);
+			try {
+				while ((zeile = in.readLine()) != null) {
+					lineArray.add(zeile);				
+				}
+				for (int i = (lineArray.size()-1); i >= 0; i--)
+				{
+					linesBackward.append(lineArray.get(i));
+					linesBackward.append(System.lineSeparator());
+				}
+				if(linesBackward.length() > 0) {
+					linesBackward.delete(linesBackward.length()-2, linesBackward.length()); // löscht das letzte NewLine
+				}
+				
+				
+				//////// HIER WERDEN KEINE ZEICHEN ANGEHÄGT /////////
+				System.out.println(linesBackward.length() + "-> Länge des zu Schreibenden Textes");
+				pw.print(linesBackward);
+				pw.flush();
+				System.out.println(writeFile.length() + "-> Länge der beschriebenen Datei");
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			finally {
+				Start.closeReader(in);
+				Start.closeWriter(pw);
+			}
+		}
+	}
+	
+	private static void einlesenUndBuchstabenweiseRückwärtsSchreiben(File readFile, File writeFile) {
+		System.out.println("EinlesenUndBuchstabenweiseRückwärtsSchreiben");
+		System.out.println(readFile.length() + "-> Länge der Ursprungsdatei");
+		if (readFile.isFile() && readFile.canRead() && writeFile.canWrite()) {
+			
+			BufferedReader in = Start.generateBufferdReader(readFile);
+			String zeile = "";
+			StringBuffer wholeTextBackward =  new StringBuffer();
+			PrintWriter pw = Start.generatePrintWriter(writeFile);
+			
+			try {
+				in.mark(100);
+				System.out.println(in.readLine() + "" + in.readLine());
+				in.reset();
+				while ((zeile = in.readLine()) != null) {
+					System.out.println(zeile.trim());
+					for (int i = (zeile.length()-1); i >= 0; i--) {
+						wholeTextBackward.append(zeile.charAt(i));
+					}
+//					in.mark(100); //Wenn Zeile länger dann Problem
+//					if (in.readLine() != null) {
+						wholeTextBackward.append(System.lineSeparator());
+//					}
+//					in.reset();
+				}
+				if(wholeTextBackward.length() > 0  ) {
+					wholeTextBackward.delete(wholeTextBackward.length()-2, wholeTextBackward.length()); // löscht das letzte NewLine
+				}
+				System.out.println(wholeTextBackward.length() + "-> Länge des zu Schreibenden Textes");
+				pw.print(wholeTextBackward);
+				pw.flush();
+				System.out.println(writeFile.length() + "-> Länge der beschriebenen Datei");
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			finally {
+				Start.closeReader(in);
+				Start.closeWriter(pw);
+			}
+		}
+	}
+
+	public static void einlesenUndSchreibenVonText(File readFile, File writeFile) {
+		System.out.println("EinlesenUndSchreibenVonText");	
+		System.out.println(readFile.length() + "-> Länge der Ursprungsdatei");
+		FileReader fr = Start.generateFileReader(readFile);
+		PrintWriter fw = Start.generatePrintWriter(writeFile);
+		if (readFile.isFile() && readFile.canRead() && writeFile.canWrite()) {
 			try {
 				char zeichen = 0;
 				char abbruch = (char) -1;
 				while ((zeichen = (char)fr.read()) != abbruch) { //abbruch seperat deklariert nötig, da read beim Ende -1 zurückgibt
 					fw.write(zeichen);
 				}
+				fw.flush();
+				System.out.println(writeFile.length() + "-> Länge der beschriebenen Datei");
 			}
 			catch (IOException e) {
 				e.printStackTrace();
 			}
 			
 			finally {
-				if (fr != null) {
-					try { fr.close(); } catch (IOException e) {	e.printStackTrace(); }
-				}
-				if ( fw != null ) {
-					try { fw.close(); } catch ( IOException e ) { e.printStackTrace(); } 
-				}
+				Start.closeReader(fr);
+				Start.closeWriter(fw);
 			}
 		}
 	}
 
-	
-	public static void gebeDateiZeichenweiseAus(File inputFile) {
-		System.out.println("Zeichenweise Ausgabe von: " + inputFile);
-		if (inputFile.isFile() && inputFile.canRead()) {
-			try {
-				FileReader fr = new FileReader(inputFile);
-				for (int i = 0; i < inputFile.length(); i++) {
-					try {
-						char c = (char)fr.read();
-						System.out.print(c);
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-				}
-					if (fr != null) {
-						try { fr.close(); } catch (IOException e) {	e.printStackTrace(); }
-					}
-			} catch (FileNotFoundException e) {
-				e.printStackTrace();
-			}
+	private static void closeReader(Reader in) {
+		if (in != null) {
+			try { in.close(); } catch (IOException e) {	e.printStackTrace(); }
 		}
-		
 	}
 	
+	private static void closeWriter(Writer w) {
+		if (w != null) {
+			try { w.close(); } catch (IOException e) {	e.printStackTrace(); }
+		}
+	}
+
+	private static BufferedReader generateBufferdReader(File inputFile) {
+		BufferedReader in = null;
+		try {
+			in = new BufferedReader(new FileReader(inputFile));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		return in;
+	}
+	
+	private static PrintWriter generatePrintWriter(File inputFile) {
+		PrintWriter pw = null;
+		try {
+			pw = new PrintWriter(new BufferedWriter(new FileWriter(inputFile)));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return pw;
+	}
+	
+	private static FileReader generateFileReader(File inputFile) {
+		try {
+			FileReader fr = new FileReader(inputFile);
+			return fr;
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public static void gebeDateiZeichenweiseAus(File inputFile) {
+		System.out.println("Zeichenweise Ausgabe von: " + inputFile);
+		System.out.println(inputFile.length());
+		if (inputFile.isFile() && inputFile.canRead()) {
+			FileReader fr = Start.generateFileReader(inputFile);
+			for (int i = 0; i < inputFile.length(); i++) {
+				try {
+					char c = (char)fr.read();
+					System.out.print(c);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			Start.closeReader(fr);
+		}
+	}
 	
 	public static void gebeDateiZeilenweiseAus(File inputFile) {
 		System.out.println("Zeilenweise Ausgabe von: " + inputFile);
 		if (inputFile.isFile() && inputFile.canRead()) {
-			BufferedReader in = null;
-			FileReader fr = null;
-			try {
-				in = new BufferedReader(fr = new FileReader(inputFile));
-			} catch (FileNotFoundException e) { e.printStackTrace(); }
+			BufferedReader in = Start.generateBufferdReader(inputFile);
 			String zeile = null;
 			try {
 				while ((zeile = in.readLine()) != null) {
@@ -105,17 +270,10 @@ public class Start {
 				}
 			} catch (IOException e) { e.printStackTrace(); }
 			finally {
-				if (in != null) {
-					try {
-						in.close(); // schließt auch FileReader, bzw. inputReader
-					}
-					catch (IOException e) {
-					}
-				}
+				Start.closeReader(in);
 			}
 		}
 	}
-	
 	
 	public static void gebeDateiByteweiseAus(File inputFile) {
 		System.out.println("Byteweise Ausgabe von: " + inputFile);
@@ -146,4 +304,57 @@ public class Start {
 			}
 		}
 	}
+	
+	public static void gebeDateiROT13DecryptedAus(File inputFile) {
+		System.out.println("ROT13 Decrypted von: " + inputFile);
+		if (inputFile.isFile() && inputFile.canRead()) {
+			FileReader fr = Start.generateFileReader(inputFile);
+			System.out.println(inputFile.length());
+			for (int i = 0; i < inputFile.length(); i++) {
+				try {
+					char c = (char)fr.read();
+					char cDecrypted = (char) (c-13);
+					System.out.print(cDecrypted);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			Start.closeReader(fr);
+		} 
+	}
+	
+	public static void gebeDateiZeichenweiseAusInHexa(File inputFile) {
+		System.out.println("Zeichenweise Ausgabe als Hexadezimalzahl von: " + inputFile);
+		System.out.println(inputFile.length());
+		if (inputFile.isFile() && inputFile.canRead()) {
+			FileReader fr = Start.generateFileReader(inputFile);
+			for (int i = 0; i < inputFile.length(); i++) {
+				try {
+					char c = (char)fr.read();
+					System.out.print(Integer.toHexString(c) + " ");
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			Start.closeReader(fr);
+		}
+	}
+	
+	private static void gebeDateiZeichenweiseAusInDezimal(File inputFile) {
+		System.out.println("Zeichenweise Ausgabe als Dezimalzahl von: " + inputFile);
+		System.out.println(inputFile.length());
+		if (inputFile.isFile() && inputFile.canRead()) {
+			FileReader fr = Start.generateFileReader(inputFile);
+			for (int i = 0; i < inputFile.length(); i++) {
+				try {
+					char c = (char)fr.read();
+					System.out.print((int) c + " ");
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			Start.closeReader(fr);
+		}
+	}
+
 }
